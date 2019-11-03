808721ad5d2459810d3aa56ded43aa2e30a4bf6c
MARIS <marisangelique@gmail.com>
2019-11-03 00:02:50 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Music from './pages/Music';
+import Art from './pages/Art';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component= {Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/music" component={Music} />
+          <Route exact path="/art" component={Art} />
+        </Switch>
+      </Router>
     </div>
-  );
+    
+  )
 }
 
 export default App;
